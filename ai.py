a1453a8aee99ef90a011939914150aa21fe2b35e
SandanSpitzhacke <victor.13@web.de>
2022-11-26 03:09:52 +0100

    Added AI

---
+++ ai.py
@@ -0,0 +1,29 @@
+import openai
+from lyrics import main
+import urllib.request
+import os
+import json
+
+openai.api_key = "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"
+images = []
+
+def generateImage(i):
+    response = openai.Image.create(
+        prompt=i,
+        n=1,
+        size="256x256",
+    )
+    image_url = response['data'][0]['url']
+    print("Generated Image for", i + ":", image_url)
+    urllib.request.urlretrieve(image_url, "images/" + str(len(images)) + ".png")
+    images.append(i)
+
+try:
+    os.mkdir("./images")
+except Exception:
+    None
+for i in main(input("Enter Song: ")):
+    if(len(i)):
+        generateImage(i)
+
+open("./images/images.json", "w").write(json.dumps({"images": images}))
