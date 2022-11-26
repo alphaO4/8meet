f9530b47e0e76f6d7604eed1863bdb2b2ed902fc
AlphaO4 <AlphaO4@protonmail.com>
2022-11-26 01:06:21 +0000

    Small updates

--- lyrics.py
+++ lyrics.py
@@ -1,26 +1,31 @@
 #! /bin/python3
-import requests
-import json
 from ytmusicapi import YTMusic
+from dotenv import dotenv_values
 import pymongo
 
 #Setup 
 ytmusic = YTMusic('headers_auth.json')
-client = pymongo.MongoClient("mongodb://localhost:27017/")
+config = dotenv_values(".env")
+mylient = pymongo.MongoClient(config["ATLAS_URI"])
+mydb = mylient[config["DB_NAME"]]
 
 def get_song_id(song):
     songid = ytmusic.search(query=song, filter='songs', limit=1)
     songid = songid[0]['videoId']
     return songid
 
-#make a Database request and check if the lyrics exist
-#if they do, return the lyrics
-#if they don't, call get_lyrics and return the lyrics
-#def check_if_lyrics_exist(song):
-#    db = client.8meet
-#    collection = db.lyrics
+def check_if_lyrics_exist(song):
+    #make a Database request and check if the lyrics exist
+    #if they do, return the lyrics
+    #if they don't, call get_lyrics and return the lyrics
+    mycol = mydb["lyrics"]
+    query = mycol.find({'title':song.lower()})
+    for x in query:
+        if (len(x['title']) > 0):
+            return(x['data'])
     
-
+    #print("Lyrics not found Song: " + song)
+    return get_lyrics(song)
 
 # Get the lyrics from the API
 def get_lyrics(song):
@@ -34,9 +39,16 @@ def get_lyrics(song):
 
 def testing():
     song = input("Enter the song: ")
-    song = '"' + song + '"'
-    lyrics = get_lyrics(song)
+    lyrics = check_if_lyrics_exist(song)
     verses = lyrics['lyrics'].split('\n')
     print(verses)
+def main(song):
+    song_lower = song.lower()
+    lyrics = check_if_lyrics_exist(song_lower)
+    if "*\r*" in lyrics:
+        print("removing \\r")
+        lyrics = lyrics.replace("*\r*", "")
+    lyrics = lyrics['lyrics'].split('\n')
+    print(lyrics)
 
-testing()
+main("Yesterday")
