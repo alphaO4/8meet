037f4a963f5d907333abe53a4de2deb2f4962e96
SandanSpitzhacke <victor.13@web.de>
2022-11-26 04:53:23 +0100

    added random video making

--- ai.py
+++ ai.py
@@ -3,9 +3,12 @@ from lyrics import main
 import urllib.request
 import os
 import json
+import moviepy.video.io.ImageSequenceClip as movieclip
+import random
 
 openai.api_key = open("./dalle-key.txt").read()
 images = []
+imagetimes = []
 
 def generateImage(i):
     response = openai.Image.create(
@@ -17,6 +20,7 @@ def generateImage(i):
     print("Generated Image for", i + ":", image_url)
     urllib.request.urlretrieve(image_url, "images/" + str(len(images)) + ".png")
     images.append(i)
+    imagetimes.append(random.random() * 15)
 
 try:
     os.mkdir("./images")
@@ -27,3 +31,22 @@ for i in main(input("Enter Song: ")):
         generateImage(i)
 
 open("./images/images.json", "w").write(json.dumps({"images": images}))
+
+#open("./images/imagetimes.txt", "w").write(imagetimes)
+
+#os.system("ffmpeg-python -f concat -i ./images/imagetimes.txt -c:v libx264 -r 30 -pix_fmt yuv420p output.mp4")
+
+
+imagefiles = []
+processed = 0
+while(len(imagetimes)):
+    print("appending frame")
+    imagefiles.append("./images/" + str(processed) + ".png")
+    imagetimes[0] -= 1
+    if(imagetimes[0] <= 0):
+        del imagetimes[0]
+        del images[0]
+        processed += 1
+print("got all frames")
+movieclip.ImageSequenceClip(imagefiles, 1).write_videofile("video.mp4")
+print("done")
