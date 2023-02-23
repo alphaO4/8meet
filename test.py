faf9c5bc89eb98fd3a6e3fbfd416b98b09af4824
alphaO4 <48568909+alphaO4@users.noreply.github.com>
2023-02-23 23:36:21 +0100

    A first step in the right correction

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 import whisper
 
-model = whisper.load_model("small.en")
+model = whisper.load_model("base.en")
 result = model.transcribe("audio.opus")
-print(result["text"])
+for segment in result['segments']:
+    text = segment['text']
+    start = round(segment['start']/60, 2)
+    end = round(segment['end']/60, 2)
+    print('text:',text,'Start:', start, 'End:', end)
+
